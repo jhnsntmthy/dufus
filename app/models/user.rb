29a7ddbd5851c8a9182c825f80094e57447422f8
Timothy Johnson <jhnsntmthy@gmail.com>
2009-12-17 09:52:46 -0500

    paperclip working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ActiveRecord::Base
   acts_as_authentic
   
   has_attached_file :avatar
-    #, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   
-  before_save :geolocate
+  # before_save :geolocate
   
   protected
   

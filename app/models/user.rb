3f62cd65ce5e433ebd9b99c7b1b5a02b1f823447
Timothy Johnson <jhnsntmthy@gmail.com>
2009-12-18 00:09:28 -0500

    bitly added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 require "geokit"
+require "bitly"
 
 class User < ActiveRecord::Base
   acts_as_authentic
@@ -17,4 +18,31 @@ class User < ActiveRecord::Base
       self.longitude = geo.lng
     end
   end
+  
+  
+  
+  # bitly = Bitly.new("jhnsntmthy", "R_0be80a3f0fbd12ff3422bd38a9584b92")
+  # 
+  #   You can then use that client to shorten or expand urls or return more information or statistics as so:
+  # 
+  #   bitly.shorten('http://www.google.com')
+  #   bitly.shorten('http://www.google.com', :history => 1) # adds the url to the api user's history
+  #   bitly.expand('wQaT')
+  #   bitly.info('http://bit.ly/wQaT')
+  #   bitly.stats('http://bit.ly/wQaT')
+  # 
+  #   Each can be used in all the methods described in the API docs, the shorten function, for example, takes a url or an 
+  #   array of urls.
+  # 
+  #   All four functions return a Bitly::Url object (or an array of Bitly::Url objects if you supplied an array as the input). 
+  #   You can then get all the information required from that object.
+  # 
+  #   u = bitly.shorten('http://www.google.com') #=> Bitly::Url
+  # 
+  #   u.long_url #=> "http://www.google.com"
+  #   u.short_url #=> "http://bit.ly/Ywd1"
+  #   u.user_hash #=> "Ywd1"
+  #   u.hash #=> "2V6CFi"
+  #   u.info #=> a ruby hash of the JSON returned from the API
+  #   u.stats #=> a ruby hash of the JSON returned from the API
 end

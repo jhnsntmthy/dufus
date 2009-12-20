d9df002312fead42374b23640ac6834fa3dd7ce9
Timothy Johnson <jhnsntmthy@gmail.com>
2009-12-20 03:28:10 -0500

    added gem oauth

--- config/environment.rb
+++ config/environment.rb
@@ -35,6 +35,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   
   config.gem 'gravtastic', :version => '>= 2.1.0'
+  config.gem "oauth"
   config.gem "twitter"
   config.gem "httparty"
   config.gem "authlogic"
